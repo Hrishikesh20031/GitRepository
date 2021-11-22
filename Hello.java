cc36929316a9ed13326a54828053b7b94ddd5e56
Hrishikesh20031 <gmy81588@gmail.com>
2021-11-22 14:45:18 +0530

    Add Programmer name,date and Title of program

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/*
+Programmer : Rushikesh
+Date : 22/11/2021
+Title : Print Hello Everyone!
+*/
+
+class Hello
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println("Hello Everyone!");
+	}
+}
